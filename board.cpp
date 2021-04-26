cca3db38de932a18a3bc1d3c20f29ef514c7a21c
BG30 <50424545+BG30@users.noreply.github.com>
2021-04-26 03:14:41 -0700

    simple working version of game

--- board.cpp
+++ board.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-const int Xsize = 50;
-const int Ysize = 50;
+const int Xsize = 10;
+const int Ysize = 10;
 
 class Board {
     private:
         char universe[Xsize][Ysize];
+        int generation;
     public:
         Board();
         void displayBoard();
@@ -13,7 +14,15 @@ class Board {
         int getY();
         void placeLife(int x, int y);
         int livingNeighbors(int x, int y);
+        void nextGeneration();
+        int getGeneration();
+        void commenceSim();
 };
+
+void Board::commenceSim(){
+    //get number of original living cells
+
+}
 
 Board::Board(){
     for(int i = 0; i < getX(); i++){
@@ -21,6 +30,54 @@ Board::Board(){
             universe[i][j] = '.';
         }
     }
+    generation = 0;
+}
+
+void Board::nextGeneration(){
+    for(int i = 0; i < getX(); i++){
+        for(int j = 0; j < getY(); j++){
+            //get number of neighbors for cell
+            int neighbors = livingNeighbors(i, j);
+            //if living cell
+            if(universe[i][j] == 'X'){
+                //kill cell if not having exactly 2 or 3 neighbors
+                if(neighbors < 2)
+                    universe[i][j] = '.';
+                else if(neighbors > 3)
+                    universe[i][j] = '.';
+            }
+            else{
+                //revive dead cell if exactly 3 neighbors
+                if(neighbors == 3)
+                    universe[i][j] = 'X';
+            }
+        }
+    }
+
+    generation++; 
+}
+
+int Board::livingNeighbors(int x, int y){
+    int neighbors = 0;
+
+    if(universe[x-1][y+1] == 'X')
+        neighbors++;
+    if(universe[x][y+1] == 'X')
+        neighbors++;
+    if(universe[x+1][y+1] == 'X')
+        neighbors++;
+    if(universe[x-1][y] == 'X')
+        neighbors++;
+    if(universe[x+1][y] == 'X')
+        neighbors++;
+    if(universe[x-1][y-1] == 'X')
+        neighbors++;
+    if(universe[x][y-1] == 'X')
+        neighbors++;
+    if(universe[x+1][y-1] == 'X')
+        neighbors++;
+    
+    return neighbors;
 }
 
 void Board::displayBoard(){
@@ -30,6 +87,7 @@ void Board::displayBoard(){
         }
         std::cout << std::endl;
     }
+    std::cout << "Generation: " << getGeneration() << std::endl;
 }
 
 int Board::getX(){
@@ -43,4 +101,8 @@ int Board::getY(){
 void Board::placeLife(int x, int y){
     if(x < getX() && x < getY())
         universe[x][y] = 'X';
+}
+
+int Board::getGeneration(){
+    return generation;
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "board.cpp"
-using namespace std;
 
 int main(){
     Board board =  Board();
     
     board.placeLife(0,0);
+    board.placeLife(0,1);
+    board.placeLife(1,0);
+    board.placeLife(1,1);
     board.displayBoard();
-     
+    board.nextGeneration();
+    board.displayBoard();
+
+
     return 0;
 }
